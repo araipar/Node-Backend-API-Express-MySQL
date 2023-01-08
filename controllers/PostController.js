e18778a5c41cc22f18ef30b6b8407ff7cadb7f1c
rai-kargo <adwitya.paramartha@kargo.tech>
2023-01-08 15:43:50 +0700

    api order by , from body parameter

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,7 @@
 import Post from "../models/PostModel.js";
 import db from "../config/Database.js";
 
-export const getPosts = async(req, res) =>{
+export const getPosts = async (req, res) => {
     try {
         const response = await Post.findAll();
         res.status(200).json(response);
@@ -10,21 +10,26 @@ export const getPosts = async(req, res) =>{
     }
 }
 
-export const getPostsWithUserName = async(req, res) =>{
-   var v_query = "SELECT  posts.post, posts.likes, users.name, posts.\"createdAt\", posts.\"updatedAt\" FROM posts left join users on posts.user_id = users.id"
+export const getPostsWithUserName = async (req, res) => {
+    var v_query = "SELECT  posts.post, posts.likes, users.name, posts.\"createdAt\", posts.\"updatedAt\" FROM posts left join users on posts.user_id = users.id"
+    
 
-    try {
-        const response = await db.query(v_query, { raw: false });
-        res.status(200).json(response);
-    } catch (error) {
-        console.log(error.message);
+    if (req.body.SortBy != undefined){
+        v_query = v_query + " ORDER BY " + req.body.SortBy.field + " "+ req.body.SortBy.order
     }
+    console.log(v_query);
+        try {
+            const response = await db.query(v_query, { raw: false });
+            res.status(200).json(response);
+        } catch (error) {
+            console.log(error.message);
+        }
 }
 
-export const createPost = async(req, res) =>{
+export const createPost = async (req, res) => {
     try {
         await Post.create(req.body);
-        res.status(201).json({msg: "Post Created"});
+        res.status(201).json({ msg: "Post Created" });
     } catch (error) {
         console.log(error.message);
     }
